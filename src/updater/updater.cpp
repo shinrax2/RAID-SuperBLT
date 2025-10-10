8e7351dee972a2798690d6eada7a96281b167db4
shinrax2 <shinra.ubu1@yahoo.de>
2025-10-10 16:51:33 +0200

    Update updater.cpp

--- src/updater/updater.cpp
+++ src/updater/updater.cpp
@@ -183,6 +183,7 @@ int main(int argc, char *argv[])
 
 		// unpack new dll
 		//raidhook::ExtractZIPArchive(DLL_UPDATE_FILE, ".");
+        MessageBox(0, "BLABLA.", "BLT Downloader", MB_OK);
 
 		//clean up
 		std::filesystem::remove(DLL_UPDATE_FILE);
